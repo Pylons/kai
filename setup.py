b2c76fb227551d49cf019372fa5f42cf77f9c09d
Ben Bangert <ben@groovie.org>
2008-12-15 16:28:42 -0800

    Adding css compressor

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     install_requires=[
         "Pylons>=0.9.7rc4", "CouchDB>=0.4", "python-openid>=2.2.1",
         "pytz>=2008i", "Babel>=0.9.4", "tw.forms==0.9.2", "docutils>=0.5",
-        "PyXML>=0.8.4",
+        "PyXML>=0.8.4", "cssutils>=0.9.6a0",
     ],
     setup_requires=["PasteScript>=1.6.3"],
     packages=find_packages(exclude=['ez_setup']),
