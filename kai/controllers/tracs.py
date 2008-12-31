7d1aa2c0a34d721a4f2db6537ac97ef6faadd9f1
Ben Bangert <ben@groovie.org>
2008-12-31 09:46:16 -0800

    Capture Trac errors and show them

--- kai/controllers/tracs.py
+++ kai/controllers/tracs.py
@@ -2,6 +2,7 @@ import logging
 
 from pylons import config, tmpl_context as c
 from pylons.controllers.util import abort
+from webhelpers.html.builder import literal
 
 # Monkey patch the lazywriter, since mercurial needs that on the stdout
 import paste.script.serve as serve
@@ -14,10 +15,11 @@ try:
     os.environ['PYTHON_EGG_CACHE'] = os.path.join(config['pylons.paths']['root'], 'egg_cache')
     import trac.web.main
     trac_app = trac.web.main.dispatch_request
+    from trac.web import HTTPException
 except:
     pass
 
-from kai.lib.base import BaseController
+from kai.lib.base import BaseController, render
 
 log = logging.getLogger(__name__)
 
@@ -27,4 +29,9 @@ class TracsController(BaseController):
             abort(404)
         if c.user:
             environ['REMOTE_USER'] = c.user.displayname
-        return trac_app(environ, start_response)
+        try:
+            return trac_app(environ, start_response)
+        except HTTPException, obj:
+            response.status_int = c.code = obj.code
+            c.message = literal(obj.detail)
+            return render('/error.mako')
