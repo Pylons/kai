3e4d89edf4a42d9c2121d4f9da359f671f0b826c
Ben Bangert <ben@groovie.org>
2009-01-22 23:42:33 -0800

    Fix routing for other download urls

--- kai/config/routing.py
+++ kai/config/routing.py
@@ -59,7 +59,8 @@ def make_map(globs=None):
     map.connect('wiki', 'http://wiki.pylonshq.com/', _static=True)
     map.connect('download', '/download/{version}', controller='download',
                 action='index', version=globs.doc_version)
-    map.redirect('/download', '/download/{version}', version=globs.versions[-1])
+    map.connect('/download', controller='download', action='index', version=globs.versions[-1])
+    map.connect('/download/', controller='download', action='index', version=globs.versions[-1])
     map.connect('cdocs', '%s/display/pylonsdocs/{page}' % wiki, _static=True)
     
     # Accounts
