b9aeaa4123a6abb8ad741c46726b1598dfd05b0c
Ben Bangert <ben@groovie.org>
2009-01-23 12:49:06 -0800

    Force code-block on highlight

--- kai/lib/highlight.py
+++ kai/lib/highlight.py
@@ -1,6 +1,9 @@
+from docutils.parsers import rst
 from pygments import highlight
 from pygments.lexers import get_lexer_by_name, get_all_lexers
 from pygments.formatters import HtmlFormatter
+
+from kai.lib.pygmentsupport import code_block
 
 __all__ = ['code_highlight']
 
@@ -11,6 +14,8 @@ for lang in get_all_lexers():
 formatter = HtmlFormatter(linenos=True, cssclass="syntax", encoding='utf-8')
 
 def code_highlight(code, truncate_lines=None):
+    # Ensure that code-block is regged properly
+    rst.directives.register_directive('code-block', code_block)
     source = code.code
     if truncate_lines:
         split_source = source.split('\n')
@@ -19,4 +24,5 @@ def code_highlight(code, truncate_lines=None):
             source.append('...')
             source = ''.join(source)
     lexer = get_lexer_by_name(code.language, stripall=True)
+    
     return highlight(source, lexer, formatter).decode('utf-8')
