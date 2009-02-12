fb41ffee2082d8d140fac14b3e0a78f7ce98d795
Ben Bangert <ben@groovie.org>
2009-02-12 09:53:14 -0800

    Updating versions

--- kai/lib/app_globals.py
+++ kai/lib/app_globals.py
@@ -25,6 +25,6 @@ class Globals(object):
         self.etag_id = md5.md5(str(datetime.now().timetuple()[3])).hexdigest()
         self.versions = ['0.8','0.8.1', '0.8.2', '0.9', '0.9.1', '0.9.2', 
                          '0.9.3', '0.9.4', '0.9.4.1', '0.9.5', '0.9.6', '0.9.6.1',
-                         '0.9.6.2', '0.9.7rc4', '0.9.7']
-        self.current_version = '0.9.7rc4'
+                         '0.9.6.2', '0.9.7rc4', '0.9.7rc5', '0.9.7']
+        self.current_version = '0.9.7rc5'
         self.doc_version = '0.9.7'
