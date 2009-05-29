cc997a7fd67306891fa5a0a73700712505286be1
Ben Bangert <ben@groovie.org>
2009-05-29 14:57:03 -0700

    Update to latest Pylons ver

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     author='Ben Bangert',
     author_email='ben@groovie.org',
     install_requires=[
-        "Pylons>=0.9.7rc4", "CouchDB>=0.4", "python-openid>=2.2.1",
+        "Pylons>=0.9.7", "CouchDB>=0.4", "python-openid>=2.2.1",
         "pytz>=2008i", "Babel>=0.9.4", "tw.forms==0.9.3", "docutils>=0.5",
         "PyXML>=0.8.4", "cssutils>=0.9.6a0", "Pygments>=1.0",
     ],
